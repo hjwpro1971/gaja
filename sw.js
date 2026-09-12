7d7e0406d769f6271f10b94f684ba1c10aaf5709
hjwpro1971 <hjwpro@gmail.com>
2026-09-12 10:28:03 +0900

    fix: change pwa start_url to track_viewer.html

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// gaja 등산지도뷰어 오프라인 캐싱 서비스 워커 (PWA Service Worker - v18)
-const CACHE_NAME = 'gaja-trail-cache-v18';
+// gaja 등산지도뷰어 오프라인 캐싱 서비스 워커 (PWA Service Worker - v19)
+const CACHE_NAME = 'gaja-trail-cache-v19';
 const CORE_ASSETS = [
   './leaflet.js',
   './leaflet.css',
