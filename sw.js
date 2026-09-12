1cc38b6074390122c52177ede9fadba12734d1d5
hjwpro1971 <hjwpro@gmail.com>
2026-09-12 14:29:45 +0900

    feat: convert off-trail distance to km if > 1000m

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// gaja 등산지도뷰어 오프라인 캐싱 서비스 워커 (PWA Service Worker - v24)
-const CACHE_NAME = 'gaja-trail-cache-v24';
+// gaja 등산지도뷰어 오프라인 캐싱 서비스 워커 (PWA Service Worker - v25)
+const CACHE_NAME = 'gaja-trail-cache-v25';
 const CORE_ASSETS = [
   './leaflet.js',
   './leaflet.css',
