cc3ae3a762260ba7438e48d14bfc3113088dd1a0
hjwpro1971 <hjwpro@gmail.com>
2026-09-12 10:04:54 +0900

    fix: ignore cached route when online and fix missing waypoints in storage catch block

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// gaja 등산지도뷰어 오프라인 캐싱 서비스 워커 (PWA Service Worker - v14)
-const CACHE_NAME = 'gaja-trail-cache-v14';
+// gaja 등산지도뷰어 오프라인 캐싱 서비스 워커 (PWA Service Worker - v15)
+const CACHE_NAME = 'gaja-trail-cache-v15';
 const CORE_ASSETS = [
   './leaflet.js',
   './leaflet.css',
